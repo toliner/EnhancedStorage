d1211a023611ca39b2e4fbe8489672d1c2a5d212
toliner <toliner@toliner.dev>
2024-09-21 20:01:00 +0900

    Storage Typeの追加

--- src/main/java/edivad/extrastorage/items/item/ItemStorageType.java
+++ src/main/java/edivad/extrastorage/items/item/ItemStorageType.java
@@ -5,14 +5,28 @@ public enum ItemStorageType
     TIER_5(256),
     TIER_6(1024),
     TIER_7(4096),
-    TIER_8(16384);
+    TIER_8(1_6384),
+    TIER_9(6_5536),
+    TIER_10(26_2144),
+    TIER_11(104_8576),
+    TIER_12(214_7483),
+    TIER_13(-1);
 
-    private String name;
+    private final String name;
     private final int capacity;
 
     ItemStorageType(int capacity) {
-        this.name = capacity + "k";
-        this.capacity = capacity * 1000;
+        if (capacity == -1) {
+            this.name = "infinite";
+            this.capacity = -1;
+        } else {
+            if (capacity < 1_0000) {
+                this.name = capacity + "k";
+            } else {
+                this.name = capacity / 1000 + "m";
+            }
+            this.capacity = capacity * 1000;
+        }
     }
 
     public String getName() {
